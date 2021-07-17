83a1c95dd506897b6b73ef4434e38bae8a5b97a4
Xia Tershi <39051261+mmm25002500@users.noreply.github.com>
2021-07-17 22:14:50 +0800

    新增pprint安裝

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ then
 	mkdir yt
 	sudo mkdir /srv/http/yt
 	sudo pacman -Syy python3 python3-pip httpd
-	pip3 install telepot
+	pip3 install telepot pprint
 	sudo systemctl start httpd
 	sudo chown USER:USER /srv/http/yt
 	echo "安裝完成... Installation Finished..."
@@ -18,7 +18,7 @@ else
 		echo "$file存在正在進行安裝..."
 		sudo apt update&&sudo apt upgrade -y
 		sudo apt install httpd python3 python3-pip
-		pip3 install telepot
+		pip3 install telepot pprint
 		sudo mkdir /var/www/html/yt
 		sudo chown USER:USER /var/www/html/yt
 		sudo service httpd start
